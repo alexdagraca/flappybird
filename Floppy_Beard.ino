3d6343d6c4270d86a62e333eb1aab1b27100f15c
DVRIX <lucashrz57@gmail.com>
2022-11-02 10:42:51 +0100

    Update Floppy_Beard.ino

--- Floppy_Beard.ino
+++ Floppy_Beard.ino
@@ -1,5 +1,16 @@
 #include <Gamebuino-Meta.h>
 
+// Les constantes
+const int Xbird = 10;
+const int Tbird = 5;
+
+// Les fonctions
+
+// Les variables
+
+int Ybird = 32;
+int SensY = 1;
+
 void setup()
 {
   gb.begin();
@@ -10,16 +21,12 @@ void loop()
   while (!gb.update())
     ;
   gb.display.clear();
+
+  // Déplacement du carré
+
+  Ybird = Ybird + SensY;
+
+  // Les dessins
+
+  gb.display.fillRect(Xbird, Ybird, Tbird, Tbird);
 }
-// Les constantes
-const int Xbird = 10;
-const int Tbird = 5;
-// Les fonctions
-
-// Les variables
-
-int Ybird = 32;
-
-// Les dessins
-
-gb.display.fillrect(Xbird, Ybird, Tbird, Tbird)
