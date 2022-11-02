1e91c7855ee27b017d06323a3ccb68b5464c7e1c
DVRIX <lucashrz57@gmail.com>
2022-11-02 10:26:33 +0100

    update

--- Floppy_Beard.ino
+++ Floppy_Beard.ino
@@ -1,9 +1,25 @@
+#include <Gamebuino-Meta.h>
+
 void setup()
 {
-  // put your setup code here, to run once:
+  gb.begin();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  while (!gb.update())
+    ;
+  gb.display.clear();
 }
+// Les constantes
+const int Xbird = 10;
+const int Tbird = 5;
+// Les fonctions
+
+// Les variables
+
+int Ybird = 32;
+
+// Les dessins
+
+gb.display.fillrect(Xbird, Ybird, Tbird, Tbird)
