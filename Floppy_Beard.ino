27f95cac036208d3f8ee0229dee177173b9f1bee
DVRIX <lucashrz57@gmail.com>
2022-11-02 12:25:34 +0100

    Update Floppy_Beard.ino

--- Floppy_Beard.ino
+++ Floppy_Beard.ino
@@ -19,6 +19,7 @@ int XRect3 = 140;
 int XRect4 = 185;
 int XRect5 = 230;
 int SensX = 1;
+int GameOn = 1;
 
 void setup()
 {
@@ -30,55 +31,73 @@ void loop()
   while (!gb.update())
     ;
   gb.display.clear();
-
-  // Déplacement du carré
-
-  if (gb.buttons.pressed(BUTTON_UP))
+  if (GameOn == 1)
   {
-    Ybird = Ybird - 15;
+    // Déplacement du carré
+
+    if (gb.buttons.pressed(BUTTON_UP))
+    {
+      Ybird = Ybird - 15;
+    }
+    else
+    {
+      Ybird = Ybird + SensY;
+    }
+
+    if (Ybird + 5 == 66)
+    {
+      Ybird = 25;
+    }
+
+    // Déplacement couple Rectangle
+
+    XRect1 = XRect1 - SensX;
+    XRect2 = XRect2 - SensX;
+    XRect3 = XRect3 - SensX;
+    XRect4 = XRect4 - SensX;
+    XRect5 = XRect5 - SensX;
+
+    // Relancer le jeu si toucher
+
+    if gb
+      .collide.rectRect(Xbird, Ybird, Tbird, Tbird, XRect1, YTuydown, 10, 20)
+      {
+        GameOn = 0;
+      }
+
+    // Les dessins
+
+    /*Balle*/
+    gb.display.setColor(YELLOW);
+    gb.display.fillRect(Xbird, Ybird, Tbird, Tbird);
+
+    /*Couple rectangle 1*/
+    gb.display.setColor(GREEN);
+    gb.display.fillRect(XRect1, YTuyup, 10, 20);
+    gb.display.fillRect(XRect1, YTuydown, 10, -15);
+
+    /*Couple rectangle 2*/
+    gb.display.fillRect(XRect2, YTuyup, 10, 20);
+    gb.display.fillRect(XRect2, YTuydown, 10, -15);
+
+    /*Couple rectangle 3*/
+    gb.display.fillRect(XRect3, YTuyup, 10, 20);
+    gb.display.fillRect(XRect3, YTuydown, 10, -15);
+
+    /*Couple rectangle 4*/
+    gb.display.fillRect(XRect4, YTuyup, 10, 20);
+    gb.display.fillRect(XRect4, YTuydown, 10, -15);
+
+    /*Couple rectangle 5*/
+    gb.display.fillRect(XRect5, YTuyup, 10, 20);
+    gb.display.fillRect(XRect5, YTuydown, 10, -15);
   }
   else
   {
-    Ybird = Ybird + SensY;
+    gb.display.print("Pressez A pour recommencer");
+    if (gb.buttons.pressed(BUTTON_A))
+    {
+      GameOn = 1;
+    }
   }
-
-  if (Ybird + 5 == 66)
-  {
-    Ybird = 25;
-  }
-
-  // Déplacement couple Rectangle
-
-  XRect1 = XRect1 - SensX;
-  XRect2 = XRect2 - SensX;
-  XRect3 = XRect3 - SensX;
-  XRect4 = XRect4 - SensX;
-  XRect5 = XRect5 - SensX;
-
-  // Les dessins
-
-  /*Balle*/
-  gb.display.setColor(YELLOW);
-  gb.display.fillRect(Xbird, Ybird, Tbird, Tbird);
-
-  /*Couple rectangle 1*/
-  gb.display.setColor(GREEN);
-  gb.display.fillRect(XRect1, YTuyup, 10, 20);
-  gb.display.fillRect(XRect1, YTuydown, 10, -15);
-
-  /*Couple rectangle 2*/
-  gb.display.fillRect(XRect2, YTuyup, 10, 20);
-  gb.display.fillRect(XRect2, YTuydown, 10, -15);
-
-  /*Couple rectangle 3*/
-  gb.display.fillRect(XRect3, YTuyup, 10, 20);
-  gb.display.fillRect(XRect3, YTuydown, 10, -15);
-
-  /*Couple rectangle 4*/
-  gb.display.fillRect(XRect4, YTuyup, 10, 20);
-  gb.display.fillRect(XRect4, YTuydown, 10, -15);
-
-  /*Couple rectangle 5*/
-  gb.display.fillRect(XRect5, YTuyup, 10, 20);
-  gb.display.fillRect(XRect5, YTuydown, 10, -15);
 }
